c747836ed695130019ac197a9f7f3272b6f50d79
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-10-01 14:50:06 +0200

    Nightly (#3)
    * Fix empty command chain causing NullPointerException (#1931)
    
    * Fix empty command chain causing NullPointerException
    
    * Add test for empty command input
    
    * Prioritise errors by chain length, build subcommands first (fix #1934) (#1935)
    
    * Fix empty command chain causing NullPointerException
    
    * Add test for empty command input
    
    * Add test for #1934; subcommand priority issue
    
    * Fix ConversionNode; process subcommands first
    
    * Fix command error priority, add test for literal suggestions
    
    * Test for subcommand priority in graph tests
    
    * configurable terminal prompt
    
    * gradle 8.3 (#1)
    
    * gradle 8.3
    
    * update checkstyle
    
    * remove default player chunk loading
    
    ---------
    
    Co-authored-by: Spanner <spanner77@protonmail.com>

--- src/main/java/net/minestom/server/command/CommandParserImpl.java
+++ src/main/java/net/minestom/server/command/CommandParserImpl.java
@@ -74,6 +74,10 @@ final class CommandParserImpl implements CommandParser {
             return nodeResults.stream().map(x -> x.node.argument()).collect(Collectors.toList());
         }
 
+        int size() {
+            return nodeResults.size();
+        }
+
         Chain() {}
 
         Chain(CommandExecutor defaultExecutor,
@@ -101,18 +105,19 @@ final class CommandParserImpl implements CommandParser {
 
         NodeResult result = parseNode(parent, chain, reader);
         chain = result.chain;
-        if (result.argumentResult instanceof ArgumentResult.Success<?>) {
-            NodeResult lastNodeResult = chain.nodeResults.peekLast();
-            Node lastNode = lastNodeResult.node;
 
+        NodeResult lastNodeResult = chain.nodeResults.peekLast();
+        if (lastNodeResult == null) return UnknownCommandResult.INSTANCE;
+        Node lastNode = lastNodeResult.node;
+
+        if (result.argumentResult instanceof ArgumentResult.Success<?>) {
             CommandExecutor executor = nullSafeGetter(lastNode.execution(), Graph.Execution::executor);
             if (executor != null) return ValidCommand.executor(input, chain, executor);
         }
         // If here, then the command failed or didn't have an executor
 
         // Look for a default executor, or give up if we got nowhere
-        NodeResult lastNode = chain.nodeResults.peekLast();
-        if (lastNode.node.equals(parent)) return UnknownCommandResult.INSTANCE;
+        if (lastNode.equals(parent)) return UnknownCommandResult.INSTANCE;
         if (chain.defaultExecutor != null) {
             return ValidCommand.defaultExecutor(input, chain);
         }
@@ -136,7 +141,7 @@ final class CommandParserImpl implements CommandParser {
             NodeResult nodeResult = new NodeResult(node, chain, (ArgumentResult<Object>) result, suggestionCallback);
             chain.append(nodeResult);
             if (suggestionCallback != null) chain.suggestionCallback = suggestionCallback;
-            if (chain.nodeResults.size() == 1) { // If this is the root node (usually "Literal<>")
+            if (chain.size() == 1) { // If this is the root node (usually "Literal<>")
                 reader.cursor(start);
             } else {
                 if (!(result instanceof ArgumentResult.Success<?>)) {
@@ -173,11 +178,11 @@ final class CommandParserImpl implements CommandParser {
                 // Assume that there is only one successful node for a given chain of arguments
                 return childResult;
             } else {
-                if (error == null) {
+                if (error == null || error.chain.size() < childResult.chain.size()) {
                     // If this is the base argument (e.g. "teleport" in /teleport) then
                     // do not report an argument to be incompatible, since the more
                     // correct thing would be to say that the command is unknown.
-                    if (!(childResult.chain.nodeResults.size() == 2 && childResult.argumentResult instanceof ArgumentResult.IncompatibleType<?>)) {
+                    if (!(childResult.chain.size() == 2 && childResult.argumentResult instanceof ArgumentResult.IncompatibleType<?>)) {
                         error = childResult;
                     }
                 }
